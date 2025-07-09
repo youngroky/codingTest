5e162b1cb583428d5fee65ed61d1b0845ef13c3e
youngroky <youngrok886@gmail.com>
2025-07-10 00:12:30 +0900

    성공 : 5901

--- week3/17837.py
+++ week3/17837.py
@@ -1,1 +1,98 @@
 # https://www.acmicpc.net/problem/17837
+
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+# 입력 - 크기(N), 말 수(K)
+N, K = map(int, input().split())
+
+# 보드판 - 0은 흰색, 1은 빨간색, 2는 파란색
+board = []
+for _ in range(N):
+    row = input().split()
+    board.append([int(x) for x in row])
+    
+# 방향 설정 (→, ←, ↑, ↓)
+dirs = [(0,1),(0,-1),(-1,0),(1,0)]
+
+# 기물들의 현재 위치 + 방향 정보를 저장할 것
+pieces = [None] * K
+
+# 기물들의 위치를 저장할 배열
+position = []
+for i in range(N):
+    row = []
+    for j in range(N):
+        # 각 칸에 deque 를 이용해서 이동할 때, 위 아래로 추가가 쉽도록 함.
+        # - 맨 앞이 제일 아래 에 있는 것, 맨 뒤가 제일 위에 있는 것 
+        row.append(deque())
+    position.append(row)
+
+
+# 기물 - 행, 열, 방향(순서대로 →, ←, ↑, ↓)
+for i in range(K):
+    r, c, d = map(int, input().split())
+    # 인덱스 때문에 하나씩 줄여서 저장
+    r -= 1; c -= 1; d -= 1
+    pieces[i] = {
+        "pos": (r, c),        # 위치 (행, 열)
+        "dir": dirs[d]        # 방향
+    }
+    position[r][c].append(i)
+
+# 말 인덱스를 받아 해당 말 위에 있는 모든 말들 deque 로 추출
+def get_pieces(piece_index):
+    x, y = pieces[piece_index]["pos"]
+    idx = position[x][y].index(piece_index)
+    moving = deque()
+    while len(position[x][y]) > idx:
+        moving.append(position[x][y].pop())
+    return moving
+
+# 말 이동 처리
+def move(piece_index):
+    x, y = pieces[piece_index]["pos"]
+    dx, dy = pieces[piece_index]["dir"]
+    mx, my = x + dx, y + dy
+
+    # 맵 벗어나거나 파란색 칸인 경우
+    if not (0 <= mx < N and 0 <= my < N) or board[mx][my] == 2:
+        dx, dy = -dx, -dy  # 방향 반전
+        pieces[piece_index]["dir"] = (dx, dy)
+        mx, my = x + dx, y + dy
+        # 반전 후에도 벗어나거나 파란색이면 이동 안 함
+        if not (0 <= mx < N and 0 <= my < N) or board[mx][my] == 2:
+            return False
+
+    # 이동할 말들 추출
+    moving = get_pieces(piece_index)
+
+    # 빨간색이면 순서 뒤집기
+    if board[mx][my] == 1:
+        moving = deque(reversed(moving))
+
+    # 말 이동
+    position[mx][my].extend(moving)
+    for m in moving:
+        pieces[m]["pos"] = (mx, my)
+
+    # 종료 조건: 말이 4개 이상 쌓이면 게임 끝
+    if len(position[mx][my]) >= 4:
+        return True
+    return False
+
+# 게임 진행
+turn = 0
+result = -1
+
+while turn < 1000:
+    turn += 1
+    for i in range(K):
+        if move(i):
+            result = turn
+            break
+    if result != -1:
+        break
+
+print(result)
--- week3/5901.py
+++ week3/5901.py
@@ -1,41 +1,78 @@
 # https://www.acmicpc.net/problem/5901
 
-# Farmer John is moving! He is trying to find the best place to build a new farm so as to minimize the amount of travel he needs to do each day.
+import sys
+import heapq
+from itertools import permutations
+input = sys.stdin.readline
 
-# The region to which FJ plans to move has N towns (1 <= N <= 10,000). 
-# There are M bi-directional roads (1 <= M <= 50,000) connecting certain pairs of towns. 
-# All towns are reachable from each-other via some combination of roads. 
-# FJ needs your help selecting the best town as the home for his new farm.
+# 일단 입력을 받아야된다. N:노드,M:길,K:마켓
+N, M, K = map(int, input().split())
 
-# There are markets in K of the towns (1 <= K <= 5) that FJ wants to visit every day. 
-# In particular, every day he plans to leave his new farm, visit the K towns with markets, and then return to his farm. 
-# FJ can visit the markets in any order he wishes. 
-# When selecting a town in which to build his new farm, 
-# FJ wants to choose only from the N-K towns that do not have markets, since housing prices are lower in those towns.
+# 마켓 위치 입력
+market = []
+for _ in range(K):
+    market.append(int(input()))
 
-# Please help FJ compute the minimum distance he will need to travel during his daily schedule, 
-# if he builds his farm in an optimal location and chooses his travel schedule to the markets as smartly as possible.
+is_market = [False] * (N + 1)
+for m in market:
+    is_market[m] = True
 
-# 입력
-# Line 1: Three space-separated integers, N, M, and K.
-# Lines 2..1+K: Line i+1 contains an integer in the range 1...N identifying the town containing the ith market. Each market is in a different town.
-# Lines 2+K..1+K+M: Each line contains 3 space-separated integers, i, j (1 <= i,j <= N), and L (1 <= L <= 1000), indicating the presence of a road of length L from town i to town j.
-'''
-5 6 3
-1
-2
-3
-1 2 1
-1 5 2
-3 2 3
-3 4 5
-4 2 7
-4 5 10
-'''
+# 길 입력받아서 그래프 완성
+graph = [[] for _ in range(N+1)]
+for _ in range(M):
+    a, b, c = map(int, input().split())
+    graph[a].append((b, c))
+    graph[b].append((a, c))
 
+# 다익스트라 알고리즘 -> 우선순위 큐를 통해 구현
+def dijkstra(start):
+    dist = [10_000] * (N+1)
+    dist[start] = 0
+    pq = [(0, start)]
+    while pq:
+        cost, node = heapq.heappop(pq)
+        # 그냥 가는거 보다 더 짧은게 있으면 패스
+        if cost > dist[node]:
+            continue
+        for next_node, w in graph[node]:
+            new_cost = cost + w
+            # 더 작으면 넣기
+            if dist[next_node] > new_cost:
+                dist[next_node] = new_cost
+                heapq.heappush(pq, (new_cost, next_node))
+    return dist
 
-# 출력
-# Line 1: The minimum distance FJ needs to travel during his daily routine, if he builds his farm in an optimal location.
-'''
-12
-'''
+# 각 마켓에서 모든 노드까지 계산
+d_market = [dijkstra(m) for m in market]
+
+# 마켓끼리 이동할 떄 최단 경로를 미리 구해놓음 -> 속도 증가를 위해서
+# 이떄 마켓 순서 0,1,2
+market_dist = [[0]*(K) for _ in range(K)]
+for i in range(K):
+    for j in range(K):
+        market_dist[i][j] = d_market[i][market[j]]
+
+# 마켓 순서대로 돌면서 찾기
+min_total_dist = 100_000
+
+for farm in range(1,N+1):
+    # 마켓있으면 넘김
+    if farm in market:
+        continue
+
+    # farm → market[i] 거리
+    to_markets = [d_market[i][farm] for i in range(K)]
+
+    # 마켓 방문 순열 생성
+    for order in permutations(range(K)):
+        total = 0
+        # 집 → 첫 마켓
+        total += to_markets[order[0]]
+        # 마켓 간 이동
+        for i in range(K - 1):
+            total += market_dist[order[i]][order[i+1]]
+        # 마지막 마켓 → 집
+        total += to_markets[order[-1]]
+        min_total_dist = min(min_total_dist, total)
+
+print(min_total_dist)
